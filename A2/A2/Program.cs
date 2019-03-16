e68a6080e9b4a7a5f8da5fa8f4bf91f1f0aca9d2
mostafa <mostafa.msd.1377@gmail.com>
2019-03-16 20:01:48 +0330

    hw4 is complete

---
+++ A2/A2/Program.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace A2
+{
+    public  class Program
+    {
+        public static void Main(string[] args)
+        {
+        }
+        public static void AssignPi(out double pi)
+            => pi = Math.PI;
+        public static void Square(ref int num)
+            => num = num * num;
+        public static void Swap(ref double num1,ref double num2)
+        {
+            double help = num1;
+            num1 = num2;
+            num2 = help;
+        }
+        public static void Sum(out int sum, params int[] nums)
+        {
+            sum = 0;
+            foreach (int num in nums)
+            {
+                sum += num;
+            }
+        }
+        public static void Append(ref int[] first,int second)
+        {
+            int[] array = first;
+            first = new int[(array.Length) + 1];
+            for(int i=0;i<array.Length;i++)
+            {
+                first[i]= array[i] ;
+            }
+            first[array.Length] = second;
+
+        }
+        public static void AbsArray(int []array)
+        {
+            for(int i=0;i<array.Length;i++)
+            {
+                if (array[i] < 0)
+                    array[i] = Math.Abs(array[i]);
+            }
+        }
+        public static void ArraySwap(int[] first,int [] second)
+        {
+            for (int i=0;i<first.Length;i++)
+            {
+                first[i] = first[i] + second[i];
+                second[i] = first[i] - second[i];
+                first[i] = first[i] - second[i];
+            }
+        }
+        public static void ArraySwap(ref int[] first,ref int[] second)
+        {
+            int[] help = first;
+            first = second;
+            second = help;
+        }
+    }
+}
---
+++ A2/A2Tests/ProgramTests.cs
@@ -0,0 +1,90 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using A2;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace A2.Tests
+{
+    [TestClass()]
+    public class ProgramTests
+    {
+        [TestMethod()]
+        public void AssignPiTest()
+        {
+            Program.AssignPi(out double pi);
+            Assert.AreEqual(Math.PI, pi);
+        }
+
+        [TestMethod()]
+        public void SquareTest()
+        {
+            int num = 2;
+            Program.Square(ref num);
+            Assert.AreEqual(4, num);
+        }
+
+        [TestMethod()]
+        public void SwapTest()
+        {
+            double num1 = 2;
+            double num2 = 4;
+            Program.Swap(ref num1, ref num2);
+            Assert.AreEqual(4, num1);
+            Assert.AreEqual(2, num2);
+        }
+
+        [TestMethod()]
+        public void SumTest()
+        {
+            int[] test = new int[] { 1, 2, 3, 4, 5 };
+            Program.Sum(out int sum, test);
+            Assert.AreEqual(15, sum);
+        }
+
+        [TestMethod()]
+        public void AppendTest()
+        {
+            int[] act = new int[] { 1, 2, 3 };
+            int second = 4;
+            int[] exp = new int[] { 1, 2, 3, 4 };
+            Program.Append(ref act, second);
+            CollectionAssert.AreEqual(exp, act);
+        }
+
+        [TestMethod()]
+        public void AbsArrayTest()
+        {
+            int[] act = new int[] { -1, 2, -3, -4, 5 };
+            int[] exp = new int[] { 1, 2, 3, 4, 5 };
+            Program.AbsArray(act);
+            CollectionAssert.AreEqual(exp, act);
+        }
+
+        [TestMethod()]
+        public void ArraySwapTest()
+        {
+            int[] first = new int[] { 1, 2, 3, 4 };
+            int[] second = new int[] { 5, 6, 7, 8 };
+            int[] actfirst = new int[] { 5, 6, 7, 8 };
+            int[] actsecond = new int[] { 1, 2, 3, 4 };
+            Program.ArraySwap(first, second);
+            CollectionAssert.AreEqual(actfirst, first);
+            CollectionAssert.AreEqual(actsecond, second);
+        }
+
+        [TestMethod()]
+        public void ArraySwapTest1()
+        {
+            int[] first = new int[] { 1, 2, 3 };
+            int[] second = new int[] { 4, 5, 6, 7, 8 };
+            int[] actfirst = new int[] { 4, 5, 6, 7, 8 };
+            int[] actsecond = new int[] { 1, 2, 3 };
+            Program.ArraySwap(ref first, ref second);
+            CollectionAssert.AreEqual(actfirst, first);
+            CollectionAssert.AreEqual(actsecond, second);
+        }
+    }
+}
