2cb35957120b8100af887b0e5d42c69c131c084d
mostafa <mostafa.msd.1377@gmail.com>
2019-04-04 14:29:15 +0430

    Product class is completed and ProductConstructorTest passed

--- A3/A3/Product.cs
+++ A3/A3/Product.cs
@@ -7,11 +7,41 @@ namespace A3
 {
     public class Product
     {
-        public string Name;
-        public float Price;
+        private string _Name;
+        private float _Price;
+        public string Name
+        {
+            get
+            {
+                return _Name;
+            }
+            set
+            {
+                if (string.IsNullOrWhiteSpace(value))
+                    throw new ArgumentNullException();
+                else
+                    _Name = value;
+            }
+        }
+        public float Price
+        {
+            get
+            {
+                return _Price;
+            }
+            set
+            {
+                if (value <= 0)
+                    Console.WriteLine("Wrong Price!");
+                else
+                    _Price = value;
+            }
+        }
 
         public Product(string name, float price)
         {
+            this.Name = name;
+            this.Price = price;
         }
     }
 }
--- A3/A3/Program.cs
+++ A3/A3/Program.cs
@@ -10,15 +10,7 @@ namespace A3
     {
         public static void Main(string[] args)
         {
-            List<int> a = new List<int>();
-            a.Add(1);
-            a.Add(2);
-            a.Add(3);
-            for (int i = 0; i < a.Count; i++)
-            {
-                Console.Write(a[i]);
-                Console.Write(" ");
-            }
+            
 
         }
     }
