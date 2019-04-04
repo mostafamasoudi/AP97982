f01f43812864d5302002a4c92e4cd9a3a04d3457
mostafa <mostafa.msd.1377@gmail.com>
2019-04-04 15:53:42 +0430

    Order class completed,OrderConstructorTest passed,CalculateTotalPriceTest passed

--- A3/A3/Order.cs
+++ A3/A3/Order.cs
@@ -8,17 +8,48 @@ namespace A3
 {
     public class Order
     {
-        public List<Product> Products;
-        public bool IsDelivered;
+        List<Product> _Products;
+        bool _IsDelivered;
+        public List<Product> Products
+        {
+            get
+            {
+                return _Products;
+            }
+            set
+            {
+                _Products = value;
+            }
+        }
+        public bool IsDelivered
+        {
+            get
+            {
+                return _IsDelivered;
+            }
+            set
+            {
+                if(value==false || value==true)
+                    _IsDelivered = value;
+                else
+                    Console.WriteLine("Bad Report For Delivering");
+            }
+        }
 
         public Order(List<Product> products, bool isDelivered)
         {
+            this.IsDelivered = isDelivered;
+            this.Products = products;
         }
 
         public float CalculateTotalPrice()
         {
-            //TODO
-            return -1;
+            float totalprice = 0;
+            for(int i=0;i<Products.Count;i++)
+            {
+                totalprice += Products[i].Price;
+            }
+            return totalprice;
         }
     }
 }
