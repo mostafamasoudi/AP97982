5289ffc7d108182c5ca4f5b641361d6727075419
mostafa <mostafa.msd.1377@gmail.com>
2019-04-06 11:39:09 +0430

    Customer class is completed,OrderConstructorTest passed,UndeliveredOrdersTest passed

--- A3/A3/Customer.cs
+++ A3/A3/Customer.cs
@@ -8,25 +8,117 @@ namespace A3
 {
     public class Customer
     {
-        public string Name;
-        public City City;
-        public List<Order> Orders;
+        string _Name;
+        City _City;
+        List<Order> _Orders;
+        public string Name
+        {
+            get
+            {
+                return _Name;
+            }
+            set
+            {
+                if (string.IsNullOrWhiteSpace(value))
+                    throw new ArgumentNullException();
+                else
+                    _Name = value;
+            }
+        }
+        public City City
+        {
+            get
+            {
+                return _City;
+            }
+            set
+            {
+                _City = value;
+            }
+        }
+        public List<Order> Orders
+        {
+            get
+            {
+                return _Orders;
+            }
+            set
+            {
+                _Orders = value;
+
+            }
+        }
 
         public Customer(string name, City city, List<Order> orders)
         {
-            //TODO
+            if (string.IsNullOrEmpty(name))
+                throw new ArgumentNullException();
+            else
+                this.Name = name;
+            if (city == null)
+                throw new ArgumentNullException();
+            else
+                this.City = city;
+            if (orders == null)
+                throw new ArgumentNullException();
+            else
+                this.Orders = orders;
+        }
+        private List<Product> ListOfExistProduct()
+        {
+            List<Product> listofproduct = new List<Product>();
+            for(int i=0;i<Orders.Count;i++)
+            {
+                for(int j=0;j<Orders[i].Products.Count;j++)
+                {
+                    bool exist = false;
+                    for(int k=0;k<listofproduct.Count;k++)
+                    {
+                        if (Orders[i].Products[j]== listofproduct[k])
+                            exist = true;
+                    }
+                    if (exist == false)
+                        listofproduct.Add(Orders[i].Products[j]);
+                }
+            }
+            return listofproduct;
         }
 
         public Product MostOrderedProduct()
         {
-            //TODO
-            return null;
+            List<Product> listofproduct = ListOfExistProduct();
+            int maxnumber = 0;
+            int IDofmaxproduct = -1;
+            for (int k = 0; k < listofproduct.Count; k++)
+            {
+                int count = 0;
+                for (int i = 0; i < Orders.Count; i++)
+                {
+                    for (int j = 0; j < Orders[i].Products.Count; j++)
+                    {
+                        if (listofproduct[k] == Orders[i].Products[j])
+                            count++;
+                    }
+                }
+                if (count >= maxnumber)
+                {
+                    maxnumber = count;
+                    IDofmaxproduct = k;
+                }
+            }
+            return listofproduct[IDofmaxproduct];
+            
         }
 
         public List<Order> UndeliveredOrders()
         {
-            //TODO
-            return null;
+            List<Order> undelivered = new List<Order>();
+            for (int i = 0; i < Orders.Count; i++)
+            {
+                if (!Orders[i].IsDelivered)
+                    undelivered.Add(Orders[i]);
+            }
+            return undelivered;
         }
     }
 }
