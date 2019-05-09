761c3ea385898f3009401e18ec31f0a1bec1c985
mostafa <mostafa.msd.1377@gmail.com>
2019-05-09 13:08:47 +0430

    HW8

---
+++ A8/A8/Human.cs
@@ -0,0 +1,60 @@
+﻿using System;
+
+namespace A8
+{
+    public class Human
+    {
+        public string FirstName;
+        public string LastName;
+        public DateTime BirthDate;
+        public int Height;
+        public Human(string firstname,string lastname, DateTime birthdate,int height)
+        {
+            this.FirstName = firstname;
+            this.LastName = lastname;
+            this.BirthDate = birthdate;
+            this.Height = height;
+        }
+        public static Human operator +(Human first, Human second) 
+            => new Human("ChildFirstName", "ChildLastName",DateTime.Today, 30);
+
+        public static bool operator >(Human first, Human second)
+            => first.BirthDate < second.BirthDate;
+
+        public static bool operator <(Human first, Human second)
+            => first.BirthDate > second.BirthDate;
+
+        public static bool operator >=(Human first, Human second)
+            => first.BirthDate <= second.BirthDate;
+
+        public static bool operator <=(Human first, Human second)
+           => first.BirthDate >= second.BirthDate;
+
+        public static bool operator ==(Human first, Human second)
+           => first.BirthDate == second.BirthDate;
+
+        public static bool operator !=(Human first, Human second)
+           => first.BirthDate != second.BirthDate;
+
+        public override bool Equals(object obj)
+        {
+            bool returnvalue ;
+            Human other = (Human)obj;
+            if ((other.BirthDate == this.BirthDate)
+                && (other.FirstName == this.FirstName)
+                && (other.LastName == this.LastName)
+                && (other.Height == this.Height))
+                returnvalue = true;
+            else
+                returnvalue = false;
+
+            return returnvalue;
+        }
+        public override int GetHashCode()
+           => this.BirthDate.GetHashCode() ^ this.FirstName.GetHashCode()
+              ^ this.LastName.GetHashCode() ^ this.Height.GetHashCode();
+
+        
+
+    }
+}
---
+++ A8/A8Tests/HumanTests.cs
@@ -0,0 +1,80 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using A8;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace A8.Tests
+{
+    [TestClass()]
+    public class HumanTests
+    {
+        Human human1 = new Human("Ali", "Hoseini", DateTime.Today, 180);
+        Human female1 = new Human("zahra", "zahrayi", DateTime.Today, 160);
+        Human human2 = new Human("Ali", "masoudi", DateTime.Today, 170);
+        Human human3 = new Human("Alireza", "ahmadi", DateTime.Today, 180);
+
+        [TestMethod()]
+        public void HumanTest()
+        {
+            Human human00 = new Human("Ali", "Hoseini", DateTime.Today, 180);
+            Assert.AreEqual(human00, human1);
+
+            Human human01 = new Human("Ali", "Hoseini", DateTime.Now, 180);
+            Assert.AreNotEqual(human01, human1);
+
+        }
+
+        [TestMethod()]
+        public void EqualsTest()
+        {
+            Human human02 = new Human("Ali", "akbari", DateTime.Today, 150);
+            bool actual= human02.Equals(human2);
+            bool expected = false;
+            Assert.AreEqual(expected, actual);
+            Human human03 = new Human("Ali", "masoudi", DateTime.Today, 170);
+            actual = human03.Equals(human2);
+            expected = true;
+            Assert.AreEqual(expected, actual);
+        }
+
+        [TestMethod()]
+        public void GetHashCodeTest()
+        {
+            Human human01 = new Human("Alireza", "ahmadi", DateTime.Today, 190);
+            Assert.AreNotEqual(human01.GetHashCode(), human3.GetHashCode());
+
+            Human human02 = new Human("Alireza", "ahmadi", DateTime.Today, 180);
+            Assert.AreEqual(human02.GetHashCode(), human3.GetHashCode());
+
+            Human human03 = new Human("Alireza", "ahmad", DateTime.Today, 180);
+            Assert.AreNotEqual(human03.GetHashCode(), human3.GetHashCode());
+
+            Human human04 = new Human("Alirezai", "ahmad", DateTime.Today, 180);
+            Assert.AreNotEqual(human03.GetHashCode(), human3.GetHashCode());
+
+        }
+
+        [TestMethod]
+        public void PlusOperatorTest()
+        {
+            Human child = human1 + female1;
+            Human expchild = new Human("ChildFirstName", "ChildLastName", DateTime.Today, 30);
+            Assert.AreEqual(expchild, child);
+        }
+
+        [TestMethod]
+        public void OtherOperatorTest()
+        {
+            Assert.AreEqual(false, human1 > human2);
+            Assert.AreEqual(false, human1 < human2);
+            Assert.AreEqual(true, human1 == human2);
+            Assert.AreEqual(true, human1 >= human2);
+            Assert.AreEqual(true, human1 <= human2);
+            Assert.AreEqual(false, human1 != human2);
+        }
+
+    }
+}
