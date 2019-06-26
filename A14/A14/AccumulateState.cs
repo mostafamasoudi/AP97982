cf7961f1f5f741da4698112e7bb7b34355fdb810
mostafa <mostafa.msd.1377@gmail.com>
2019-06-26 10:21:42 +0430

    HW14

---
+++ A14/A14/AccumulateState.cs
@@ -0,0 +1,37 @@
+﻿using static System.Console;
+
+namespace A14
+{
+    public class AccumulateState : CalculatorState
+    {
+        public AccumulateState(Calculator calc) : base(calc) { }
+
+        // #7 لطفا //
+        public override IState EnterEqual()
+        {
+            this.Calc.Evalute();
+            this.Calc.UpdateDisplay();
+            return new ComputeState(this.Calc);
+        }
+        public override IState EnterZeroDigit() => EnterNonZeroDigit('0');
+        public override IState EnterNonZeroDigit(char c)
+        {
+            // #8 لطفا! //
+            this.Calc.Display += c;
+            return this;
+        }
+
+        // #9 لطفا! //
+        public override IState EnterOperator(char c)
+        {
+            this.Calc.Evalute();
+            return new ComputeState(this.Calc);
+        }
+        public override IState EnterPoint()
+        {
+            // #10 لطفا! //
+            this.Calc.Display += '.';
+            return new PointState(this.Calc);
+        }
+    }
+}
---
+++ A14/A14/ErrorState.cs
@@ -0,0 +1,24 @@
+﻿namespace A14
+{
+    /// <summary>
+    /// ماشین حساب وقتی به این حالت وارد میشود که خطایی رخ داده باشد
+    /// از این حالت هر کلیدی که فشار داده شود به وضعیت اولیه باید برگردیم
+    /// #2 لطفا!
+    /// </summary>
+    public class ErrorState : CalculatorState
+    {
+        public ErrorState(Calculator calc) : base(calc) { }
+        public override IState EnterEqual() => Reset();
+        public override IState EnterNonZeroDigit(char c) => Reset();
+        public override IState EnterZeroDigit() => Reset();
+        public override IState EnterOperator(char c) => Reset();
+        public override IState EnterPoint() => Reset();
+
+        public IState Reset()
+        {
+            this.Calc.Clear();
+            return new StartState(this.Calc);
+        }
+
+    }
+}
